aa2d97eb5e03683dd3a9b16fdf7e0b9658e1b80c
Michael Neely <michael@michaelneely.net>
2023-02-15 10:43:25 -0500

    Added the cancel movie button functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,17 +6,26 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 
-const close
-
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
 }
+
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
     toggleBackdrop();
 };
 
-startAddMovieButton.addEventListener('click', toggleMovieModal);
+cancelAddMovie = () => {
+    toggleMovieModal();
+}
 
+const backdropClickHandler = () => {
+    toggleMovieModal();
+}
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', toggleMovieModal);
 
