bc15f0e19c5b64e8e3975593f1a1ff6bbda3e88a
Michael Neely <michael@michaelneely.net>
2023-02-17 08:56:06 -0500

    Added the 'Add movie' and 'delete movie' functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,9 +14,47 @@ const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 // const userInputs = addMovieModal.getElementByTagName('input');
+const entryTextSection = document.getElementById('entry-text');
 
 const movies =[];
+const updateUI = () => {
+    if (movies.length === 0) {
+        entryTextSection.style.display = 'block';
+    } else {
+        entryTextSection.style.display = 'none';
+    }
+};
 
+const deleteMovieHandler = (movieId) => {
+    let movieIndex = 0;
+    for(const movie of movies) {
+        if ( movie.id === movieId) {
+            break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex, 1);
+    const listRoot = document.getElementById('movie-list');
+    listRoot.children[movieIndex].remove();
+    // listRoot.removeChild(listRoot.children[movieIndex]);
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+        <img src="${imageUrl}" alt="${title}"">
+    </div>
+    <div class="movie-element__info">
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>
+    `;
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id) )
+    const listRoot = document.getElementById('movie-list');
+    listRoot.append(newMovieElement);
+};
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
     toggleBackdrop();
@@ -49,6 +87,7 @@ const addMovieHandler = () => {
     }
 
     const newMovie = {
+        id: Math.random().toString(),
         title: titleValue,
         image: imgUrlValue,
         rating:ratingValue
@@ -57,6 +96,8 @@ const addMovieHandler = () => {
     console.log(movies);
     toggleMovieModal();
     clearMovieInput();
+    renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating );
+    updateUI();
 };
 
 const backdropClickHandler = () => {
